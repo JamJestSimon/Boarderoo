a5f4a11ac70ac07d462afe4f5d702b08a7e8577e
AKUCHARSKI1 <adriank210@interia.pl>
2025-01-17 13:14:10 +0100

    + order details

--- boarderoo-front-end/src/app/order-details/order-details.component.ts
+++ boarderoo-front-end/src/app/order-details/order-details.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ToastContainerDirective, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-order-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './order-details.component.html',
   styleUrl: './order-details.component.css'
 })
 export class OrderDetailsComponent {
-
+  @Output() close = new EventEmitter<void>(); 
+  @Input() selectedOrder?: any;
+  onClose() {
+    this.close.emit(); // Emitowanie zdarzenia
+  }
 }
--- boarderoo-front-end/src/app/orders-list/orders-list.component.ts
+++ boarderoo-front-end/src/app/orders-list/orders-list.component.ts
@@ -1,17 +1,20 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
 import { CommonModule } from '@angular/common';
+import { OrderDetailsComponent } from '../order-details/order-details.component';
 
 @Component({
   selector: 'app-orders-list',
   standalone: true,
-  imports: [NavBarComponent, CommonModule],
+  imports: [NavBarComponent, CommonModule, OrderDetailsComponent],
   templateUrl: './orders-list.component.html',
   styleUrl: './orders-list.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA], // Dodajemy schemat
 })
 export class OrdersListComponent {
   isAdmin = true;
+  isOrderDetailsVisible = false;
+  selectedOrder = "";
   orders = [
     { number: '#12345', date: '2025-01-15', status: 'Oczekujące', amount: '250,00 PLN', statusClass: 'status-pending' },
     { number: '#12346', date: '2025-01-14', status: 'Zrealizowane', amount: '300,00 PLN', statusClass: 'status-completed' },
@@ -32,4 +35,14 @@ export class OrdersListComponent {
     { number: '#12346', date: '2025-01-14', status: 'Zrealizowane', amount: '300,00 PLN', statusClass: 'status-completed' },
     { number: '#12347', date: '2025-01-15', status: 'Anulowane', amount: '150,00 PLN', statusClass: 'status-canceled' },
   ];
+
+
+  toggleOrderDetails(order?: any) {
+    if (order) {
+      this.selectedOrder = order; // Ustawiamy wybraną kartę
+    }
+    this.isOrderDetailsVisible = !this.isOrderDetailsVisible;
+    console.log("togle" + this.isOrderDetailsVisible);
+  }
+
 }
